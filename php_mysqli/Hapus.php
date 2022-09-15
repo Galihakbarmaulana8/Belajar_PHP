d1c138480df1256ec1b88c097c03df2bf45deec7
Galih akbar maulana <110004091+Galihakbarmaulana8@users.noreply.github.com>
2022-09-15 14:34:16 +0700

    Delete php_mysqli directory

--- php_mysqli/Hapus.php
+++
@@ -1,5 +0,0 @@
-<?php 
-include "koneksiMysqli.php";
-$conn->query("delete from siswa where nis='".$_GET['nis']."'");
-header("location:data.php")
-?>
--- php_mysqli/koneksiMysqli.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = new mysqli("localhost","root","","sekolahh");
-mysqli_connect_errno();
-
-//$conn -> query("insert into siswa(nis,nama,kelas) values('1111','Santy','Kelas 10') ");
-?>
